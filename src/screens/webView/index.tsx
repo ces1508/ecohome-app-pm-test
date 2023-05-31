6131b1e529c65a0d262bd812ea9eca8081d5086c
christian.segura@segurosbolivar.com- Desarrollador IT <christian.segura@segurosbolivar.com>
2023-05-30 19:47:34 -0500

    fix: se ajusta el back con el nativo

--- src/screens/webView/index.tsx
+++ src/screens/webView/index.tsx
@@ -1,25 +1,57 @@
-import React, {useContext, useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useContext, useState, useRef, useEffect} from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+  Dimensions,
+  BackHandler,
+} from 'react-native';
 import {WebView} from 'react-native-webview';
-import {Dimensions} from 'react-native';
 import {PMEvent} from '../../components';
 import {EmbedContext} from '../../context/embed';
 import {showToast} from '../../utils/toast';
 
-const {width, height} = Dimensions.get('screen');
+const {height} = Dimensions.get('screen');
 
 export const WebViewScreen = () => {
+  // usar ref causa un problema en la primera vez que ingresa
+  const [viewportHeight, setViewportHeight] = useState(0);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+  const [showMessage, setShowMessage] = useState<any>(null);
+
+  const webViewRef = useRef<WebView>(null);
+
   const {
     state: {url, saving},
   } = useContext(EmbedContext);
-
-  const [showMessage, setShowMessage] = useState<any>(null);
 
   const handleMessage = (message: any) => {
     setShowMessage(JSON.parse(message.nativeEvent.data));
   };
 
   const handleClose = () => setShowMessage(null);
+
+  const handleRefreshPage = () => {
+    if (webViewRef.current && !refreshing) {
+      webViewRef.current.reload();
+      setRefreshing(false);
+    }
+  };
+
+  const handleBack = () => {
+    let backWebView = false;
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      backWebView = true;
+    }
+    return backWebView;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', handleBack);
+    return () =>
+      BackHandler.removeEventListener('hardwareBackPress', handleBack);
+  }, []);
 
   const handleError = () => {
     showToast({
@@ -30,37 +62,49 @@ export const WebViewScreen = () => {
   };
 
   return (
-    <View style={styles.wrapper}>
+    <ScrollView
+      style={styles.wrapper}
+      onLayout={e => setViewportHeight(e.nativeEvent.layout.height)}
+      refreshControl={
+        <RefreshControl
+          enabled={!refreshing}
+          refreshing={refreshing}
+          onRefresh={handleRefreshPage}
+        />
+      }>
       {showMessage && (
         <PMEvent
           event={{
-            title: showMessage.type,
-            message: showMessage.message,
+            title: showMessage?.type ?? 'type no set',
+            message: showMessage?.message ?? 'message no set',
           }}
           handleClose={handleClose}
         />
       )}
       {!saving && (
         <WebView
+          ref={webViewRef}
+          pullToRefreshEnabled={false}
           source={{uri: url}}
-          originWhitelist={['*']}
           onMessage={handleMessage}
           onError={handleError}
+          allowsBackForwardNavigationGestures={true}
           setSupportMultipleWindows={false}
           style={{
-            width,
-            height,
+            flex: 1,
+            width: '100%',
+            height: viewportHeight,
           }}
         />
       )}
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   wrapper: {
     position: 'relative',
-    width,
-    height: height - 60,
+    width: '100%',
+    height: '100%',
   },
 });
